c84b1a4bbc05afcf4e52961d901e75a3b182e812
PolinaK4 <p.kaliuzhnaya@gmail.com>
2023-02-14 22:28:31 +0100

    Changed name to day

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿Console.Write("Hi! What’s your name? ");
-var name = Console.ReadLine();
-Console.WriteLine($"Nice to meet you, {name}!");
+﻿Console.Write("Hi! What’s day today? ");
+var day = Console.ReadLine();
+Console.WriteLine($"{day} is a nice day!");
