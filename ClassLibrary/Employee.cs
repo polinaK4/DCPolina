6b4b5aac0caf79a4387c9110d581d4020392f1c6
PolinaK4 <p.kaliuzhnaya@gmail.com>
2023-03-23 00:46:43 +0100

    Task with classes

---
+++ ClassLibrary/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml.Linq;
+
+namespace ClassLibrary
+{
+   
+    public class Employee
+    {
+        public string firstName = "Undefined";
+        public string lastName = "Undefined";
+        public int age;
+        public int experienceYears;
+        public string position = "Intern";
+               
+        public Employee()  // 1 конструктор
+        {
+            firstName = "New";
+            lastName = "Employee"; 
+
+        }
+        public Employee(string firstName, string lastName, int age) // 2 конструктор
+        { 
+            this.firstName = firstName; 
+            this.lastName = lastName;
+            this.age = age;
+
+        } 
+        public Employee(string firstName, string lastName, int age, int experienceYears, string position) // 3 конструктор
+        { 
+            this.firstName = firstName; 
+            this.lastName = lastName; 
+            this.age = age; 
+            this.experienceYears = experienceYears; 
+            this.position = position; 
+        } 
+
+        public void Print()
+        {
+            Console.WriteLine($"First Name: {firstName} | Last Name: {lastName} | Age: {age} | Experience Years: {experienceYears} | Position: {position}");
+        }
+
+        public void VacationDays()
+        {
+            if (experienceYears >= 10)
+            {
+                Console.WriteLine("26 vacation days");
+            }
+            else
+                Console.WriteLine($"20 vacation days. Years left before adding extra days: {10 - experienceYears}");
+
+        }
+
+    }
+    
+}
---
+++ MainProject/Factory.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using ClassLibrary;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{    static void Main(string[] args)
+    {
+        Factory iTechArt = new Factory
+        {
+            factoryName = "iTechArt",
+            city = "Krakow",
+            employees = new Employee[]
+            {
+                new Employee { firstName = "John", lastName = "Doe", age = 23 },
+                new Employee { firstName = "Helena", lastName = "Bug", age = 20 },
+                new Employee { firstName = "Harry", lastName = "Potter", age = 19 },
+                new Employee { firstName = "Hanna", lastName = "Smith", age = 27, experienceYears = 5, position = "middle" },
+                new Employee { firstName = "Clark", lastName = "Kent", age = 30, experienceYears = 10, position = "senior" },
+                new Employee { firstName = "Elon", lastName = "Musk", age = 22, experienceYears = 1, position = "junior" },
+                new Employee { firstName = "Barak", lastName = "Obama", age = 35, experienceYears = 15, position = "senior" },
+            } 
+        };
+
+        Factory Tesla = new Factory
+        {
+            factoryName = "Tesla",
+            city = "Warsaw",
+            officeNumber = 3,
+            employees = new Employee[]
+            {
+                new Employee { firstName = "Donald", lastName = "Tusk", age = 20 },
+                new Employee { firstName = "Kate", lastName = "Smith", age = 18 },
+                new Employee { firstName = "July", lastName = "Trump", age = 2, experienceYears = 5, position = "middle" },
+                new Employee { firstName = "James", lastName = "Jobs", age = 30, experienceYears = 4, position = "middle" },
+                new Employee { firstName = "Alex", lastName = "Taylor", age = 21, experienceYears = 1, position = "junior" },
+            }
+        };
+               
+        iTechArt.ReturnNumberEmployees();
+        iTechArt.AddEmployee();
+        iTechArt.ListEmployees();
+        iTechArt.ListEmployeesByPosition();
+    }
+}
+public class Factory
+    {
+        public string factoryName = "Undefined";
+        public string city = "Undefined";
+        public int officeNumber = 1;
+        public Employee[] employees;
+                 
+        public Factory() //конструктор1
+        {
+            factoryName = "New Company";                       
+        }
+        public Factory(string factoryName, string city, int officeNumber) //конструктор2
+    {
+        this.factoryName = factoryName;
+        this.city = city;
+        this.officeNumber = officeNumber;
+    }
+    public void ReturnNumberEmployees()
+    {
+        Console.WriteLine($"The number of employees in {factoryName}: {employees.Length}");
+    }
+        public void AddEmployee()
+    {
+        Array.Resize(ref employees, employees.Length + 1);
+        employees[employees.Length - 1] = new Employee { firstName = "Drako", lastName = "Malfoy", age = 19 };
+        Console.WriteLine($"New employee added to {factoryName}: {employees[employees.Length - 1].firstName} {employees[employees.Length - 1].lastName} age: {employees[employees.Length - 1].age} experience years: {employees[employees.Length - 1].experienceYears} position: {employees[employees.Length - 1].position}");
+
+    }
+    public void ListEmployees()
+    {
+        Console.WriteLine($"List of employees in {factoryName}:");
+        for (int i = 0; i < employees.Length; i++)
+        {
+            Console.WriteLine($"{employees[i].firstName} {employees[i].lastName}");
+        }
+    }
+    public void ListEmployeesByPosition()
+    {
+        Console.WriteLine($"Type position to get the list of employees (intern, junior, middle, senior):");
+        string typePosition = Console.ReadLine();
+        Console.WriteLine($"List of employees in {factoryName} on position '{typePosition}':");
+        for (int i = 0; i < employees.Length; i++)
+        {
+            if (employees[i].position == typePosition)
+            {
+                Console.WriteLine($"{employees[i].firstName} {employees[i].lastName}");
+            }            
+        }
+    }
+
+
+}
