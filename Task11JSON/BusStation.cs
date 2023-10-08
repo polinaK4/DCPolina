b2d71d44d3cf515f65e3ab3043e175011ff04e2f
PolinaK4 <p.kaliuzhnaya@gmail.com>
2023-10-08 23:19:24 +0200

    Task 12 Json

---
+++ Task11JSON/BusStation.cs
@@ -0,0 +1,19 @@
+﻿namespace Task12JSON
+{
+    public class BusStation
+    {
+        public string color;
+        public int number;
+        public bool active;
+        public DateTime datetimeOfCheck;
+        public List<Bus> buses;
+        public BusStation(string color, int number, bool active, DateTime datetimeOfCheck, List<Bus> buses)
+        {
+            this.color = color;
+            this.number = number;
+            this.active = active;
+            this.datetimeOfCheck = datetimeOfCheck;
+            this.buses = buses;
+        }
+    }
+}
